e5050bef9a3f8fc4318406ba1289d75af52c62d3
mihai23bogdan <99962239+mihai23bogdan@users.noreply.github.com>
2022-03-29 13:18:07 +0300

    Create scriptclean.sh

---
+++ scriptclean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+container=$(docker ps -a --filter "name=nodejs" --format '{{.Names}}')
+
+
+# Stop and delete containers
+if [ -z "$container" ] ; then echo "No containers to delete"; else echo "The following container(s) will be deleted: "; docker stop $container && docker rm $container ; fi
