2bae8a19e26970d37841b70bcb0024d82eb8c8a4
Mihai Bogdan <99962239+mihai23bogdan@users.noreply.github.com>
2022-04-20 12:01:45 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,9 +1,23 @@
 def buildApp() {
   echo 'BUILDING THE APPLICATION'
+  sh 'docker build -t ${dockerimage}:${BUILD_NUMBER} .'
 }
 
 def startingApp() {
   echo 'START THE APP'
+}
+
+def scriptpermimisionApp() {
+  sh 'chmod +x ./scriptclean.sh'
+}
+
+def executescriptApp() {
+  sh './scriptclean.sh'
+}
+
+def dockerrunApp() {
+  sh 'docker run -p ${dockerports} -d --name ${dockercontainer}${BUILD_NUMBER} ${dockerimage}:${BUILD_NUMBER}'
+  echo "deploying version ${params.VERSION}"
 }
 
 return this
